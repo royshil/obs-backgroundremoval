fd0f8e8cb9180e7b5ce465fa0f86e9e3b31bcaf2
Kaito Udagawa <umireon@kaito.tokyo>
2025-11-06 14:10:29 +0900

    Update DelayLoad.cpp

--- src/DelayLoad.cpp
+++ src/DelayLoad.cpp
@@ -10,28 +10,27 @@ std::filesystem::path binaryPath;
 
 extern "C" void PluginLoaded()
 {
-    binaryPath = obs_get_module_binary_path(obs_current_module());
-    obs_log(LOG_INFO, "Plugin binary path: %s", binaryPath.string().c_str());
+	binaryPath = obs_get_module_binary_path(obs_current_module());
+	obs_log(LOG_INFO, "Plugin binary path: %s", binaryPath.string().c_str());
 }
 
 extern "C" void PluginUnloaded()
 {
-    // Do nothing
+	// Do nothing
 }
 
 #ifdef _WIN32
 extern "C" {
-FARPROC WINAPI DelayLoadHook(
-    unsigned dliNotify,
-    PDelayLoadInfo pdli
-) {
-    if (dliNotify == dliNotePreLoadLibrary) {
-        obs_log(LOG_INFO, "Delay loading DLL: %s", pdli->szDll);
-	    std::filesystem::path absPath = std::filesystem::absolute(binaryPath / ".." / PLUGIN_NAME / pdli->szDll);
-        obs_log(LOG_INFO, "Resolved DLL path: %s", absPath.string().c_str());
-        return (FARPROC)LoadLibraryExW(absPath.c_str(), NULL, LOAD_WITH_ALTERED_SEARCH_PATH);
-    }
-    return NULL;
+FARPROC WINAPI DelayLoadHook(unsigned dliNotify, PDelayLoadInfo pdli)
+{
+	if (dliNotify == dliNotePreLoadLibrary) {
+		obs_log(LOG_INFO, "Delay loading DLL: %s", pdli->szDll);
+		std::filesystem::path absPath =
+			std::filesystem::absolute(binaryPath / ".." / PLUGIN_NAME / pdli->szDll);
+		obs_log(LOG_INFO, "Resolved DLL path: %s", absPath.string().c_str());
+		return (FARPROC)LoadLibraryExW(absPath.c_str(), NULL, LOAD_WITH_ALTERED_SEARCH_PATH);
+	}
+	return NULL;
 }
 
 const PfnDliHook __pfnDliNotifyHook2 = DelayLoadHook;
