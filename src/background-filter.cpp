92d111dd030405f39b07df5214f9803d0d9e4662
Kaito Udagawa <umireon@kaito.tokyo>
2025-11-11 03:57:53 +0900

    Remove TensorRT support and references
    This commit removes all code, localization strings, and documentation references related to TensorRT support. The affected files include source code, constants, and multiple locale files, reflecting the discontinuation of TensorRT as an inference device option.

--- src/background-filter.cpp
+++ src/background-filter.cpp
@@ -153,13 +153,10 @@ obs_properties_t *background_filter_properties(void *data)
 							    OBS_COMBO_TYPE_LIST, OBS_COMBO_FORMAT_STRING);
 
 	obs_property_list_add_string(p_use_gpu, obs_module_text("CPU"), USEGPU_CPU);
-#if defined(__linux__) && defined(__x86_64__)
-	obs_property_list_add_string(p_use_gpu, obs_module_text("GPUTensorRT"), USEGPU_TENSORRT);
-#endif
-#ifdef HAVE_ONNXRUNTIME_CUDA
+#ifdef HAVE_ONNXRUNTIME_CUDA_CP
 	obs_property_list_add_string(p_use_gpu, obs_module_text("GPUCuda"), USEGPU_CUDA);
 #endif
-#ifdef HAVE_ONNXRUNTIME_ROCM
+#ifdef HAVE_ONNXRUNTIME_ROCM_EP
 	obs_property_list_add_string(p_use_gpu, obs_module_text("GPURocm"), USEGPU_ROCM);
 #endif
 #if defined(__APPLE__)
