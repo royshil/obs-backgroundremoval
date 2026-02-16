7d3a33f2856135303342797711cbf346b21f0ce4
Kaito Udagawa <umireon@kaito.tokyo>
2026-02-17 00:28:30 +0900

    Update build_ort_windows.ps1

--- scripts/build_ort_windows.ps1
+++ scripts/build_ort_windows.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = 'Stop'
 
 $ORT_VERSION = "v1.24.1"
 $CONFIGURATION = "Release"
-$ORT_COMPONENTS = @( "onnxruntime_session", "onnxruntime_optimizer", "onnxruntime_providers", "onnxruntime_lora", "onnxruntime_framework", "onnxruntime_graph", "onnxruntime_util", "onnxruntime_mlas", "onnxruntime_common", "onnxruntime_flatbuffers", "onnxruntime_providers_dml", "onnxruntime_providers_cuda" )
+# $ORT_COMPONENTS = @( "onnxruntime_session", "onnxruntime_optimizer", "onnxruntime_providers", "onnxruntime_lora", "onnxruntime_framework", "onnxruntime_graph", "onnxruntime_util", "onnxruntime_mlas", "onnxruntime_common", "onnxruntime_flatbuffers", "onnxruntime_providers_dml", "onnxruntime_providers_cuda" )
 
 $ROOT_DIR = Convert-Path .
 $DEPS_DIR = Join-Path $ROOT_DIR ".deps_vendor"
@@ -14,8 +14,6 @@ if (!(Test-Path $ORT_SRC_DIR)) {
 	git clone --depth 1 --branch $ORT_VERSION https://github.com/microsoft/onnxruntime.git $ORT_SRC_DIR
 	Set-Location $ORT_SRC_DIR
 	git submodule update --init --recursive --depth 1
-	# Copy-Item cmake\CMakeLists.txt cmake\CMakeLists.txt.orig
-	# "macro(install)`nendmacro()`n$(Get-Content cmake\CMakeLists.txt.orig -Raw)" | Set-Content cmake\CMakeLists.txt -NoNewline
 }
 
 $BUILD_PY = Join-Path $ORT_SRC_DIR "tools\ci_build\build.py"
@@ -26,32 +24,26 @@ if (!(Test-Path $ORT_BUILD_DIR)) {
 		--config $CONFIGURATION `
 		--update `
 		--parallel `
-		--targets $($ORT_COMPONENTS -join ' ') `
 		--compile_no_warning_as_error `
 		--cmake_extra_defines CMAKE_POLICY_VERSION_MINIMUM=3.5 `
-		--disable_rtti `
-		--enable_reduced_operator_type_support `
-		--include_ops_by_config "$ROOT_DIR/data/models/required_operators_and_types.with_runtime_opt.config" `
-		--parallel `
-		--skip_onnx_tests `
+		--use_vcpkg `
 		--skip_submodule_sync `
 		--skip_tests `
-		--use_vcpkg `
-		--update `
+		--include_ops_by_config "$ROOT_DIR/data/models/required_operators_and_types.with_runtime_opt.config" `
+		--enable_reduced_operator_type_support `
+		--disable_rtti
 }
 
-& python tools/ci_build/build.py `
+& python3 $BUILD_PY `
 	--build_dir "$ORT_BUILD_DIR" `
-	--cmake_generator "Visual Studio 17 2022" `
-	--cmake_extra_defines CMAKE_POLICY_VERSION_MINIMUM=3.5 `
 	--config $CONFIGURATION `
-	--disable_rtti `
-	--enable_reduced_operator_type_support `
-	--include_ops_by_config "$ROOT_DIR/data/models/required_operators_and_types.with_runtime_opt.config" `
+	--update `
 	--parallel `
-	--skip_onnx_tests `
+	--compile_no_warning_as_error `
+	--cmake_extra_defines CMAKE_POLICY_VERSION_MINIMUM=3.5 `
+	--use_vcpkg `
 	--skip_submodule_sync `
 	--skip_tests `
-	--use_vcpkg `
-	--build `
-	--targets $($ORT_COMPONENTS -join ' ') cpuinfo kleidiai `
+	--include_ops_by_config "$ROOT_DIR/data/models/required_operators_and_types.with_runtime_opt.config" `
+	--enable_reduced_operator_type_support `
+	--disable_rtti
