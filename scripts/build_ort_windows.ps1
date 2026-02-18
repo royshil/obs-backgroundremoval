f168f1e7ef4a89c1187c3879003f9da85afcaac7
Kaito Udagawa <umireon@kaito.tokyo>
2026-02-18 11:05:00 +0900

    Update build_ort_windows.ps1

--- scripts/build_ort_windows.ps1
+++ scripts/build_ort_windows.ps1
@@ -41,9 +41,10 @@ $commonArgs = @(
 	"--config", "$CONFIGURATION",
 	"--parallel",
 	"--compile_no_warning_as_error",
-	"--use_cache",
-	"--cmake_generator", "Ninja",
-	"--cmake_extra_defines", "CMAKE_POLICY_VERSION_MINIMUM=3.5",
+	"--cmake_extra_defines",
+	"CMAKE_POLICY_VERSION_MINIMUM=3.5",
+	"CMAKE_C_COMPILER_LAUNCHER=ccache",
+	"CMAKE_CXX_COMPILER_LAUNCHER=ccache"
 	"--use_vcpkg",
 	"--skip_submodule_sync",
 	"--skip_tests",
