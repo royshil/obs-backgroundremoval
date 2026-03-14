7985d388fd1212fdbfdcb51f94a245e0e7fff102
Kaito Udagawa <umireon@kaito.tokyo>
2026-03-14 15:31:31 +0900

    Remove interactive-help
    Signed-off-by: Kaito Udagawa <umireon@kaito.tokyo>

--- pages/astro.config.mjs
+++ pages/astro.config.mjs
@@ -1,9 +1,5 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import svelte from "@astrojs/svelte";
-
 // https://astro.build/config
-export default defineConfig({
-  integrations: [svelte()],
-});
+export default defineConfig({});
